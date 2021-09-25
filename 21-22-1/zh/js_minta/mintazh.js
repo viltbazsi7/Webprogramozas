07f8e3b43f2b506eb17732e17d51ff680f7ca570
PotatoMaster <20043803+PotatoMaster@users.noreply.github.com>
2021-09-25 18:32:31 +0200

    Apró hibák javítása a minta zhban

--- 21-22-1/zh/js_minta/mintazh.js
+++ 21-22-1/zh/js_minta/mintazh.js
@@ -22,26 +22,22 @@ let helyszinek = [
 ];
 let ul = document.querySelector('ul');
 
-function toggleSelection(event) {
-    event.target.classList.toggle('selected');
-}
-
 for (let helyszin of helyszinek) {
     let li = document.createElement('li');
     li.innerText = helyszin;
     li.dataset.helyszin = helyszin;
-    //li.addEventListener('click', toggleSelection);
+    //li.addEventListener('click', () => li.classList.toggle('kijelolve'));
     ul.appendChild(li);
 }
 delegal(ul, 'li', 'click', (esemeny, li) => {
-    li.classList.toggle('selected');
+    li.classList.toggle('kijelolve');
 });
 
 let nevinput = document.querySelector('#nev');
 let nevgomb = document.querySelector('#nevgomb');
 
 nevgomb.addEventListener('click', () => {
-    document.querySelectorAll('.selected').forEach(elem => {
+    document.querySelectorAll('.kijelolve').forEach(elem => {
         elem.innerText = nevinput.value;
     });
 });
@@ -51,7 +47,7 @@ let szingomb = document.querySelector('#szingomb');
 
 szingomb.addEventListener('click', () => {
     let szin = szininput.value;
-    document.querySelectorAll('.selected').forEach(elem => {
+    document.querySelectorAll('.kijelolve').forEach(elem => {
         if (szin == 'piros') {
             elem.style.color = 'red';
         } else if (szin == 'zöld') {
@@ -79,7 +75,7 @@ szingomb.addEventListener('click', () => {
 let reset = document.querySelector('#reset');
 
 reset.addEventListener('click', () => {
-    document.querySelectorAll('.selected').forEach(elem => {
+    document.querySelectorAll('.kijelolve').forEach(elem => {
         elem.style.color = 'black';
         elem.innerText = elem.dataset.helyszin;
     });
