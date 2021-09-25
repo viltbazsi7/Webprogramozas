68cdd5029d85020ddc8cb1c1e1d972a57ae81386
PotatoMaster <20043803+PotatoMaster@users.noreply.github.com>
2021-09-25 18:28:23 +0200

    JS minta zh átkerült a saját mappájába + estis 3. házi hiányzó képei pótolva

---
+++ 21-22-1/zh/js_minta/mintazh.js
@@ -0,0 +1,86 @@
+// Ezt a delegálás függvényt egy-az-egyben felhasználhatjátok
+function delegal(szulo, gyerek, mikor, mit) {
+    function esemenyKezelo(esemeny) {
+        let esemenyCelja    = esemeny.target;
+        let esemenyKezeloje = this;
+        let legkozelebbiKeresettElem = esemenyCelja.closest(gyerek);
+        if (esemenyKezeloje.contains(legkozelebbiKeresettElem)) {
+            mit(esemeny, legkozelebbiKeresettElem);
+        }
+    }
+    szulo.addEventListener(mikor, esemenyKezelo);
+}
+
+let helyszinek = [
+    'Etyek',
+    'Érd',
+    'III. Kerület',
+    'Margit-sziget',
+    'Normafa',
+    'Pusztafalu',
+    'XI. Kerület'
+];
+let ul = document.querySelector('ul');
+
+function toggleSelection(event) {
+    event.target.classList.toggle('selected');
+}
+
+for (let helyszin of helyszinek) {
+    let li = document.createElement('li');
+    li.innerText = helyszin;
+    li.dataset.helyszin = helyszin;
+    //li.addEventListener('click', toggleSelection);
+    ul.appendChild(li);
+}
+delegal(ul, 'li', 'click', (esemeny, li) => {
+    li.classList.toggle('selected');
+});
+
+let nevinput = document.querySelector('#nev');
+let nevgomb = document.querySelector('#nevgomb');
+
+nevgomb.addEventListener('click', () => {
+    document.querySelectorAll('.selected').forEach(elem => {
+        elem.innerText = nevinput.value;
+    });
+});
+
+let szininput = document.querySelector('#szin');
+let szingomb = document.querySelector('#szingomb');
+
+szingomb.addEventListener('click', () => {
+    let szin = szininput.value;
+    document.querySelectorAll('.selected').forEach(elem => {
+        if (szin == 'piros') {
+            elem.style.color = 'red';
+        } else if (szin == 'zöld') {
+            elem.style.color = 'green';
+        } else if (szin == 'kék') {
+            elem.style.color = 'blue';
+        }
+        // vagy
+        /*
+        switch (szin) {
+            case 'piros':
+                elem.style.color = 'red';
+                break;
+            case 'zöld':
+                elem.style.color = 'green';
+                break;
+            case 'kék':
+                elem.style.color = 'blue';
+                break;
+        }
+        */
+    });
+});
+
+let reset = document.querySelector('#reset');
+
+reset.addEventListener('click', () => {
+    document.querySelectorAll('.selected').forEach(elem => {
+        elem.style.color = 'black';
+        elem.innerText = elem.dataset.helyszin;
+    });
+});
